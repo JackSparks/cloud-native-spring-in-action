7934b576dd535af55041b1152a7a8551aa561f54
JackSparks <JackSparks@users.noreply.github.com>
2024-06-20 10:46:20 -0400

    Update CatalogServiceApplicationTests.java

--- Chapter03/03-end/catalog-service/src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
+++ Chapter03/03-end/catalog-service/src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
@@ -11,7 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CatalogServiceApplicationTests {
-
+// test
     @Autowired
     private WebTestClient webTestClient;
 
